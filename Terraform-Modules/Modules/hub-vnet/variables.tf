01a739faf2c12f572b6c13c0228c430cfd5b09a8
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-03 11:02:17 +0100

    chagend to list(string)

--- Terraform-Modules/Modules/hub-vnet/variables.tf
+++ Terraform-Modules/Modules/hub-vnet/variables.tf
@@ -24,21 +24,21 @@ variable "subnet_name" {
 }
 
 variable "address_space" {
-  type        = string
+  type        = list(string)
   description = "The address space for the Virtual Network."
 }
 
 variable "gateway_subnet_prefix" {
-  type        = string
+  type        = list(string)
   description = "Address prefix for the GatewaySubnet."
 }
 
 variable "firewall_subnet_prefix" {
-  type        = string
+  type        = list(string)
   description = "Address prefix for the AzureFirewallSubnet."
 }
 
 variable "management_subnet_prefix" {
-  type        = string
+  type        = list(string)
   description = "Address prefix for the Management Subnet."
 }
