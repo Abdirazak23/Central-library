240951798f783d1731269d2000baadc364198771
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-04 11:35:18 +0100

    vnet_name_spoke

--- Terraform-Modules/Modules/spoke-vnet/outputs.tf
+++ Terraform-Modules/Modules/spoke-vnet/outputs.tf
@@ -3,7 +3,7 @@ output "vnet_id" {
   description = "The ID of the hub virtual network."
 }
 
-output "vnet_name" {
+output "vnet_name_spoke" {
   value       = azurerm_virtual_network.hub-vnet.name
   description = "The name of the hub virtual network."
 }
