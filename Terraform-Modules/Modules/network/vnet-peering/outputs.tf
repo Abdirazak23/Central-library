40e7a318712a445efc45fe22c423959e541ed23b
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-12 22:34:25 +0100

    output test

--- Terraform-Modules/Modules/network/vnet-peering/outputs.tf
+++ Terraform-Modules/Modules/network/vnet-peering/outputs.tf
@@ -7,3 +7,8 @@ output "spoke_to_hub_peering_id" {
   value       = azurerm_virtual_network_peering.spoke_to_hub.id
   description = "The ID of the peering from Spoke to Hub."
 }
+
+output "hub_rg_name" {
+  description = "The resource group name of the hub network"
+  value       = azurerm_resource_group.rg.name # Use the actual resource group identifier from inside this module
+}
